9105ca7c87e96bdb31b5b545b4b6861a4d9bda4b
Briankim254 <kimutaibrian560@gmail.com>
2023-11-24 16:00:00 +0300

    Add new files and delete old files

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,18 +1,18 @@
-import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
-import prisma from "@/prisma/client";
- 
-const createissueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
-}); 
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
+import { issueSchema } from '../../validationSchemas';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 
-export  async function POST(request: NextRequest) {
+export async function POST(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return NextResponse.json({}, { status: 401 });
+
   const body = await request.json();
-  const validation = createissueSchema.safeParse(body);
-  if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  }
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,38 +1,18 @@
-'use client'
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import {useForm, Controller} from "react-hook-form";
-import "easymde/dist/easymde.min.css";
-import { data } from "autoprefixer";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+const IssueForm = dynamic(
+  () => import('@/app/issues/_components/IssueForm'),
+  { 
+    ssr: false,
+    loading: () => <IssueFormSkeleton />
+  }
+);
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
-
-function NewIssuePage() {
-  const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+const NewIssuePage = () => {
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(async(data)=>{
-      axios.post('/api/issues', data);
-      router.push('/issues');
-    })}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register('title')}/>
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        defaultValue=""
-        render={({ field }) => <SimpleMDE  placeholder="Description" className="rounded-lg" {...field}/> }
-      />
-      <Button>Submit New Issue</Button>
-    </form>
-  );
+    <IssueForm />
+  )
 }
 
-export default NewIssuePage;
+export default NewIssuePage
